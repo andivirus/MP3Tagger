442dfb310cd86e7cb91411ac0af0d7d46bb3a649
andivirus <andivirus@gmail.com>
2015-07-08 23:40:33 +0200

    fixed jfilechooser

--- src/com/andivirus/tagGUI.java
+++ src/com/andivirus/tagGUI.java
@@ -3,6 +3,7 @@ package com.andivirus;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 /**
  * Created by andrey on 07.07.15.
@@ -15,22 +16,43 @@ public class tagGUI {
     private JCheckBox coverCheckBox;
     private JLabel coverImageLabel;
     private JPanel tagPanel;
+    private JButton runButton;
 
     public tagGUI() {
         pathButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                if (actionEvent.getSource()== pathButton) {
-                    JFileChooser chooser = new JFileChooser();
-                    chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                    chooser.setVisible(true);
+                JFileChooser chooser = new JFileChooser();
+                chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                int returnval = chooser.showOpenDialog(null);
+                File file = chooser.getSelectedFile();
+                if(returnval==JFileChooser.APPROVE_OPTION) {
+                    pathTextField.setText(file.getAbsolutePath());
                 }
+                else{
+                    pathTextField.setText("Du musst nen Pfad auswaehlen du Nase");
+                }
+
 
             }
         });
     }
 
     public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+
         JFrame frame = new JFrame("tagGUI");
         frame.setContentPane(new tagGUI().tagPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
