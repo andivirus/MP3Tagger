852ac131fcf3cb0bc6339a7b8f4e318f6249ae5c
andivirus <andivirus@gmail.com>
2015-07-10 00:18:05 +0200

    added image preview remade argument passing to main class

--- src/com/andivirus/tagGUI.java
+++ src/com/andivirus/tagGUI.java
@@ -1,8 +1,14 @@
 package com.andivirus;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.datatransfer.FlavorEvent;
+import java.awt.datatransfer.FlavorListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 
 /**
@@ -21,6 +27,8 @@ public class tagGUI {
 
     public tagGUI() {
         tagger = new Main();
+        setCoverLabel();
+
         pathButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
@@ -28,15 +36,51 @@ public class tagGUI {
                 chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
                 int returnval = chooser.showOpenDialog(null);
                 File file = chooser.getSelectedFile();
-                if(returnval==JFileChooser.APPROVE_OPTION) {
+                if (returnval == JFileChooser.APPROVE_OPTION) {
                     pathTextField.setText(file.getAbsolutePath());
                     runButton.setEnabled(true);
-                }
-                else{
+                } else {
                     pathTextField.setText("Du musst nen Pfad auswaehlen du Nase");
                 }
 
 
+            }
+        });
+
+        try{
+            Toolkit.getDefaultToolkit().getSystemClipboard().addFlavorListener(new FlavorListener() {
+                @Override
+                public void flavorsChanged(FlavorEvent e) {
+                    setCoverLabel();
+                }
+
+
+            });
+        }
+        catch(IllegalArgumentException e){
+            System.out.println("LEL");
+        }
+
+        coverImageLabel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                setCoverLabel();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
             }
         });
 
@@ -46,24 +90,25 @@ public class tagGUI {
                 String path = pathTextField.getText();
                 String artist = artistTextField.getText();
                 String album = albumTextField.getText();
-                String argumentstring = path;
+                String[] arguments = new String[5];
 
+                arguments[0] = "-op=" + path;
+                if (!artist.isEmpty()) {
+                    arguments[1] = "-ar=" + artist;
+                }
+                if (!album.isEmpty()) {
+                    arguments[2] = "-al=" + album;
+                }
+                if (coverCheckBox.isEnabled()) {
+                    arguments[3] = "-c";
+                }
 
-                if(!artist.isEmpty()){
-                    argumentstring = argumentstring + " -ar=" + artist;
-                }
-                if(!album.isEmpty()){
-                    argumentstring = argumentstring + " -al=" + album;
-                }
-                if(coverCheckBox.isEnabled()){
-                    argumentstring = argumentstring + " -c";
-                }
-                String[] strings = new String[5];
-                strings = argumentstring.split(" -");
-                for (String s: strings){
+                for (String s : arguments) {
                     System.out.println(s);
                 }
-                //tagger.setOperations();
+
+                tagger.setOperations(arguments);
+                tagger.execute();
             }
         });
     }
@@ -91,6 +136,20 @@ public class tagGUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void setCoverLabel(){
+        coverImageLabel.setIcon(null);
+        BufferedImage bufferedImage = tagger.getImageFromClipboard();
+        if(bufferedImage != null) {
+            ImageIcon img = new ImageIcon(bufferedImage);
+            img.setImage(img.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT));
+            coverImageLabel.setText("");
+            coverImageLabel.setIcon(img);
+        }
+        else{
+            coverImageLabel.setText("No picture in Clipboard");
+        }
     }
 
 }
